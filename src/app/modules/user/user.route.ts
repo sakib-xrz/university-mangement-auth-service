589defce7ed666c88bda23825935ba5f6521d6ad
sakib-xrz <sakibxrz21@gmail.com>
2023-06-13 22:03:12 +0600

    create zod validation

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,8 +1,14 @@
+import validateRequest from '../../middlewares/validateRequest'
 import { UserController } from './user.controller'
 import express from 'express'
+import { UserValidation } from './user.validation'
 
 const router = express.Router()
 
-router.post('/create-user', UserController.createUserToDatabase)
+router.post(
+  '/create-user',
+  validateRequest(UserValidation.createUserZodSchema),
+  UserController.createUserToDatabase
+)
 
 export const UserRoute = router
